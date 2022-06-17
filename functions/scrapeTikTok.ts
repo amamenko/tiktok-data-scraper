df84283c72bb30bcbe02e88434c3a95ad672d441
amamenko <amamenko@hotmail.com>
2022-06-17 01:09:33 -0400

    Updated gitignore and added log-in functionality to puppeteer script

--- functions/scrapeTikTok.ts
+++ functions/scrapeTikTok.ts
@@ -24,7 +24,7 @@ export const scrapeTikTok = async () => {
     });
     const page = await browser.newPage();
 
-    await page.goto("https://live-backstage.tiktok.com", {
+    await page.goto("https://live-backstage.tiktok.com/login?loginType=email", {
       waitUntil: "networkidle2",
     });
 
@@ -32,7 +32,42 @@ export const scrapeTikTok = async () => {
 
     await page.screenshot({ path: "test.png" });
 
-    await page.waitForTimeout(5000);
+    try {
+      await page.click("button.semi-button-secondary");
+    } catch (e) {
+      console.error("No log in button found!");
+    }
+
+    await page.focus('input[placeholder="Enter email address"]');
+    await page.keyboard.type(process.env.TIK_TOK_EMAIL);
+    await page.focus('input[placeholder="Enter password"]');
+    await page.keyboard.type(process.env.TIK_TOK_PASSWORD);
+    try {
+      await page.click("button.semi-button-tertiary.semi-button-size-large");
+    } catch (e) {
+      console.error("No log in button found!");
+    }
+    const findByTextAndClick = async (query: string) => {
+      try {
+        const [button] = await page.$x(
+          `//span.semi-navigation-item-text[contains(., '${query}')]`
+        );
+        if (button) {
+          await button.click();
+        }
+      } catch (e) {
+        console.error(`No ${query} section button found!`);
+      }
+    };
+
+    findByTextAndClick("Hosts");
+    findByTextAndClick("LIVE now");
+
+    await page.waitForTimeout(10000);
+
+    await page.screenshot({ path: "test1.png" });
+
+    await browser.close();
   } catch (e) {
     console.error(`Received error durring Puppeteer process: ${e}`);
   }
