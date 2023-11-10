43abd7070244364e699f738008f73488891abfa5
Avi Mamenko <avimamenko@gmail.com>
2023-11-10 01:53:34 -0500

    Removed debugging console.log statements

--- components/Room/RefreshCountdownTimer.tsx
+++ components/Room/RefreshCountdownTimer.tsx
@@ -12,16 +12,11 @@ export const RefreshCountdownTimer = ({
 
   useEffect(() => {
     if (liveData?.refreshAt && !endDate) {
-      console.log({
-        refreshAt: liveData.refreshAt,
-        dateRefreshAt: new Date(liveData.refreshAt),
-      });
       changeEndDate(new Date(liveData.refreshAt));
     }
   }, [endDate, liveData?.refreshAt]);
 
   if (endDate) {
-    console.log({ endDate });
     return (
       <Countdown
         date={endDate}
