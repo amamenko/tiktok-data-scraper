44e8248ffdc0d0cff77efbeefd501586e73095c3
Avi Mamenko <avimamenko@gmail.com>
2024-03-01 02:00:40 -0500

    Added logic to filter out excluded user IDs from previous week top 100 lives

--- app/functions/getTop100LiveResults.ts
+++ app/functions/getTop100LiveResults.ts
@@ -42,6 +42,24 @@ export const getTop100LiveResults = async (
       ])
       .toArray();
     dailyLiveLives.sort((a, b) => b.diamonds - a.diamonds);
+    if (process.env.EXCLUDED_USER_IDS) {
+      try {
+        const parsedExcludedUserIds = JSON.parse(process.env.EXCLUDED_USER_IDS);
+        if (
+          Array.isArray(parsedExcludedUserIds) &&
+          parsedExcludedUserIds.length > 0
+        ) {
+          parsedExcludedUserIds.forEach((excludedUserId: string) => {
+            const index = dailyLiveLives.findIndex(
+              (live) => live.userID === excludedUserId
+            );
+            if (index > -1) dailyLiveLives.splice(index, 1);
+          });
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
     const topHundredLives = dailyLiveLives.slice(0, 100);
     const previousWeekStart = getWeekStart(1);
     const foundPreviousWeekDoc = await db
